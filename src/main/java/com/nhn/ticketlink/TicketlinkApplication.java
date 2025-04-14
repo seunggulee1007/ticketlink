f9c661cbdfb85c5f2d9002c3cf2668b73ec70cac
이승구 <leesg107@naver.com>
2025-04-14 22:49:16 +0900

    feat: first commit

---
+++ src/main/java/com/nhn/ticketlink/TicketlinkApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.ticketlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketlinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhn/ticketlink/TicketlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.ticketlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
